65edbb8e9800023d57d963d02d415f0e078cd78f
katr1234 <43642913+katr1234@users.noreply.github.com>
2019-09-25 08:49:26 +0200

    finish homework

---
+++ ContactList/ContactList/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList
+{
+    public class Contact
+    {
+        [Required]
+        public Int32 id { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        [Required]
+        public string email { get; set; }
+    }
+}
---
+++ ContactList/ContactList/Controllers/ValuesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactList.Controllers
+{
+    [ApiController]
+    [Route("contacts")]
+    public class ContactListController : ControllerBase
+    {
+        private static readonly List<Contact> contact = new List<Contact>();
+
+        // GET ALL: api/contact-list
+        [HttpGet]
+        public IActionResult GetAllItems()
+        {
+            return Ok(contact);
+        }
+
+        // GET: api/contact-list/string
+        [HttpGet]
+        [Route("{name}")]
+        public IActionResult GetItem(string name)
+        {
+            if (name != null || name.Length != 0)
+            {
+                foreach(Contact c in contact)
+                {
+                    if (c.firstName.Equals(name) || c.lastName.Equals(name))
+                    {
+                        // 200: Successful operation
+                        return Ok(c);
+                    }
+                }
+                
+            }
+            // 400: Invalid ID supplied
+            return BadRequest("Invalid ID supplied!");
+        }
+
+        // POST: api/contact-list
+        [HttpPost]
+        public IActionResult AddItem([FromBody] Contact newContact)
+        {
+            contact.Add(newContact);
+            return CreatedAtRoute("GetSpecificItem", new { index = contact.IndexOf(newContact) }, newContact);
+        }
+
+        // DELETE: api/contact-list/id
+        [Route("{index}")]
+        public IActionResult DeleteItem(int index)
+        {
+            if (index >= 0)
+            {
+                foreach (Contact c in contact)
+                {
+                    if (c.id == index)
+                    {
+                        // 204: Successful operation
+                        contact.RemoveAt(index);
+                        return NoContent();
+                    }
+                }
+                // 404: Person not found
+                return BadRequest("Person not found!");
+            }
+            return BadRequest("Invalid ID supplied!");
+        }
+    }
+}
